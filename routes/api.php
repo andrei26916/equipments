804065712d433529f35d6b8153fb59553681da99
andrei26916 <andrey.stosh@yandex.ru>
2023-02-13 19:28:58 +0800

    (*) README.md (*) api route

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/user/login', [AuthController::class, 'auth']);
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::get('/equipment', [EquipmentController::class, 'index']);
